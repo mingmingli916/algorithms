7e1765d68385b6a8e8d0b451f65f728f0af7239d
Mingming Li <mingmingli916@gmail.com>
2022-04-03 18:48:22 -0400

    Squares of a Sorted Array

--- array/Find Numbers with Even Number of Digits.py
+++ array/Find Numbers with Even Number of Digits.py
@@ -4,7 +4,6 @@ Given an array nums of integers, return how many of them contain an even number 
 
 
 Example 1:
-
 Input: nums = [12,345,2,6,7896]
 Output: 2
 Explanation:
@@ -14,8 +13,8 @@ Explanation:
 6 contains 1 digit (odd number of digits).
 7896 contains 4 digits (even number of digits).
 Therefore only 12 and 7896 contain an even number of digits.
-Example 2:
 
+Example 2:
 Input: nums = [555,901,482,1771]
 Output: 1
 Explanation:
