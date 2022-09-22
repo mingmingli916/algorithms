e706931b13879a657906e1ed4acac600bd40c8b3
Mingming Li <mingmingli916@gmail.com>
2022-09-22 18:27:18 -0400

    DFS Template

--- queque_and_stack/DFS Template.py
+++ queque_and_stack/DFS Template.py
@@ -1,4 +1,8 @@
 def DFS(cur, target, visited: set) -> bool:
+    """
+    It used the system stack.
+    The recursion uses the system stack.
+    """
     if cur == target:
         return True
     for nei in cur.neighbors:
