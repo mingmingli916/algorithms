fd61321ec2112a702def42c82b3e424e48234d58
Mingming Li <mingmingli916@gmail.com>
2022-09-29 18:47:25 -0400

    Python strings

---
+++ python/strings/006 String Validators.py
@@ -0,0 +1,85 @@
+"""
+Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+
+str.isalnum()
+This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+
+>>> print 'ab123'.isalnum()
+True
+>>> print 'ab123#'.isalnum()
+False
+str.isalpha()
+This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+
+>>> print 'abcD'.isalpha()
+True
+>>> print 'abcd1'.isalpha()
+False
+str.isdigit()
+This method checks if all the characters of a string are digits (0-9).
+
+>>> print '1234'.isdigit()
+True
+>>> print '123edsd'.isdigit()
+False
+str.islower()
+This method checks if all the characters of a string are lowercase characters (a-z).
+
+>>> print 'abcd123#'.islower()
+True
+>>> print 'Abcd123#'.islower()
+False
+str.isupper()
+This method checks if all the characters of a string are uppercase characters (A-Z).
+
+>>> print 'ABCD123#'.isupper()
+True
+>>> print 'Abcd123#'.isupper()
+False
+Task
+
+You are given a string .
+Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Input Format
+
+A single line containing a string S.
+
+Constraints
+0 <len(S) < 1000
+
+Output Format
+
+In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+In the third line, print True if S has any digits. Otherwise, print False.
+In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if S has any uppercase characters. Otherwise, print False.
+
+Sample Input
+
+qA2
+Sample Output
+
+True
+True
+True
+True
+True
+"""
+if __name__ == '__main__':
+    s = input()
+    flag = [False] * 5
+    for i in s:
+        if i.isalnum():
+            flag[0] = True
+        if i.isalpha():
+            flag[1] = True
+        if i.isdigit():
+            flag[2] = True
+        if i.isalpha() and i.islower():
+            flag[3] = True
+        if i.isalpha() and i.isupper():
+            flag[4] = True
+    for f in flag:
+        print(f)
