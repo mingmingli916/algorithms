bcb54986ee3b153b5b3c88a51497ed058cc41553
Mingming Li <mingmingli916@gmail.com>
2022-10-01 10:19:03 -0400

    Pacman A*

---
+++ artificial_inteligence/search/004 Pacman A*.py
@@ -0,0 +1,124 @@
+"""
+In the previous game, you performed UCS on the PacMan grid. In this game we use AStar algorithm to reduce the nodes expanded using search using a simple yet efficient heuristic.
+
+AStar on graphs uses the following function
+
+cost = d(s,c) + h(c)
+where s is the source node, c is the node currently expanded and h(c) is the estimation of the cost to reach from c to the destination ( food ).
+
+In this game, we use manhattan heuristic as an estimate. Given two nodes (r,c) and (r1,c1). The manhattan heuristic is the manhattan distance between the two nodes and is given by
+
+|r1 - r | + |c1 - c|
+Input Format
+
+The first line contains 2 space separated integers which is the position of the PacMan.
+The second line contains 2 space separated integers which is the position of the food.
+The third line of the input contains 2 space separated integers. Indicating the size of the rows and columns respectively.
+This is followed by row (r) lines each containing column (c) characters. A wall is represented by the character '%' ( ascii value 37 ), PacMan is represented by UpperCase alphabet 'P' ( ascii value 80 ), empty spaces which can be used by PacMan for movement is represented by the character '-' ( ascii value 45 ) and food is represented by the character '.' ( ascii value 46 )
+
+The top left of the grid is indexed (0,0) and the bottom right of the grid is indexed (r-1,c-1)
+
+The grid is indexed as per matrix convention
+
+For the sake of uniformity across all codes, cost to reach a neighboring node
+
+0 if a food is present.
+1 otherwise.
+Output Format
+
+Each cell in the grid is represented by its position in the grid (x,y). PacMan can move only UP, DOWN, LEFT or RIGHT. Your task is to print all the nodes in the shortest path calculated using Astar search between Pacman and Food.
+
+ %
+%--
+ -
+In the above cell, LEFT and UP are invalid moves. You can either go RIGHT or DOWN. RIGHT is populated first followed by DOWN. i.e., populate the queue UP, LEFT, RIGHT and DOWN order so that UP gets popped first from the queue.
+
+Print the distance 'D' between the source 'P' and the destination '.' calculated using Astar. D+1 lines follow, each line having a node encountered between 'P' and '.' both included. D+1 lines essentially representing the path between source and the destination.
+
+Sample Input
+
+35 35
+35 1
+37 37
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%-------%-%-%-----------%---%-----%-%
+%-%%%%%%%-%-%%%-%-%%%-%%%-%%%%%%%-%-%
+%-------%-------%-%-----%-----%-%---%
+%%%%%-%%%%%-%%%-%-%-%-%%%-%%%%%-%-%%%
+%---%-%-%-%---%-%-%-%---%-%---%-%---%
+%-%%%-%-%-%-%%%-%%%%%-%%%-%-%%%-%%%-%
+%-------%-----%---%---%-----%-%-%---%
+%%%-%%%%%%%%%-%%%%%%%-%%%-%%%-%-%-%-%
+%-------------%-------%-%---%-----%-%
+%-%-%%%%%-%-%%%-%-%-%%%-%-%%%-%%%-%-%
+%-%-%-----%-%-%-%-%-----%---%-%-%-%-%
+%-%-%-%%%%%%%-%-%%%%%%%%%-%%%-%-%%%-%
+%-%-%-%-----%---%-----%-----%---%---%
+%%%-%%%-%-%%%%%-%%%%%-%%%-%%%-%%%%%-%
+%-----%-%-%-----%-%-----%-%---%-%-%-%
+%-%-%-%-%-%%%-%%%-%%%-%%%-%-%-%-%-%-%
+%-%-%-%-%-----------------%-%-%-----%
+%%%-%%%%%%%-%-%-%%%%%-%%%-%-%%%-%%%%%
+%-------%-%-%-%-----%---%-----%-%---%
+%%%%%-%-%-%%%%%%%%%-%%%%%%%%%%%-%-%%%
+%---%-%-----------%-%-----%---%-%---%
+%-%%%-%%%%%-%%%%%%%%%-%%%%%-%-%-%%%-%
+%-%---%------%--------%-----%-------%
+%-%-%-%%%%%-%%%-%-%-%-%-%%%%%%%%%%%%%
+%-%-%---%-----%-%-%-%-------%---%-%-%
+%-%-%%%-%%%-%-%-%-%%%%%%%%%-%%%-%-%-%
+%-%---%-%---%-%-%---%-%---%-%-%-----%
+%-%%%-%%%-%%%%%-%%%-%-%-%%%%%-%-%%%%%
+%-------%---%-----%-%-----%---%-%---%
+%%%-%-%%%%%-%%%%%-%%%-%%%-%-%%%-%-%%%
+%-%-%-%-%-%-%-%-----%-%---%-%---%-%-%
+%-%-%%%-%-%-%-%-%%%%%%%%%-%-%-%-%-%-%
+%---%---%---%-----------------%-----%
+%-%-%-%-%%%-%%%-%%%%%%%-%%%-%%%-%%%-%
+%.%-%-%-------%---%-------%---%-%--P%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Sample Output
+
+sample output
+
+In this example, PacMan is at the position (35,35) and the food is at the position (35,1). The AStar path length between (35,35) and (35,1) is 210. All the nodes encountered between (35,35) and (35,1) both included is printed in the next 211 lines.
+"""
+import copy
+import heapq
+
+
+def a_star(pac_r, pac_c, food_r, food_c, rows, cols, grid):
+    visited = set()
+    direction = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+    h = []
+    heapq.heappush(h, (0, [pac_r, pac_c, []]))
+    while h:
+        prio, cor_and_route = heapq.heappop(h)
+        cur_r, cur_c, _ = cor_and_route
+        route = copy.deepcopy(_)
+        route.append(f'{cur_r} {cur_c}')
+
+        if cur_r == food_r and cur_c == food_c:
+            print(len(route) - 1)
+            for i in route:
+                print(i)
+            return
+
+        for r, c in direction:
+            nr, nc = cur_r + r, cur_c + c
+            if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
+                continue
+            if (grid[nr][nc] == '-' or grid[nr][nc] == '.') and (nr, nc) not in visited:
+                visited.add((nr, nc))
+                if nr == food_r and nc == food_c:
+                    heapq.heappush(h, (prio, [nr, nc, route]))
+                else:
+                    heapq.heappush(h, (prio + 1, [nr, nc, route]))
+
+
+if __name__ == '__main__':
+    pac_r, pac_c = list(map(int, input().split()))
+    food_r, food_c = list(map(int, input().split()))
+    rows, cols = list(map(int, input().split()))
+    grid = [[j for j in input().strip()] for i in range(rows)]
+    a_star(pac_r, pac_c, food_r, food_c, rows, cols, grid)
